7f132c51a63f81c971a254660a99fe86578af79a
Brandon Johnson <brandon.johnson@bluemedora.com>
2023-07-13 09:14:46 -0400

    feat: Add timezone parameter to file source (#1271)
    * helper for parse operator
    
    * add timezone
    
    * add tests
    
    * inline return
    
    * add license
    
    * lint errors
    
    * bump version
    
    * change description, add comment for function, make constants
    
    * return empty string if nothing renders instead of panicing
    
    * map[string]any -> map[string]string where applicable

---
+++ resources/helpers/operatorhelper/operators.go
@@ -0,0 +1,142 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package operatorhelper contains the helper functions for stanza operators
+package operatorhelper
+
+import (
+	"bytes"
+	"fmt"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+// Use the a special config for jsoniter, because
+// we want the keys to be sorted so the output is stable for tests
+var operatorMarshalConfig = jsoniter.Config{
+	SortMapKeys: true,
+}.Froze()
+
+// Parse formats
+const (
+	jsonParseFormat  = "json"
+	regexParseFormat = "regex"
+)
+
+// timestamp formats
+const (
+	iso8601TimestampFormat = "ISO8601"
+	rfc3339TimestampFormat = "RFC3339"
+	manualTimestampFormat  = "Manual"
+	epochTimestampFormat   = "Epoch"
+)
+
+// BpRenderStandardParsingOperator renders a parsing operator for stanza-based OTEL receivers as a line of inline YAML.
+// This helper function should only be used when parseFormat is not "none", otherwise an empty object is returned
+func BpRenderStandardParsingOperator(
+	parseFormat string,
+	parseTo string,
+	regexPattern string,
+	parseTimestamp bool,
+	timestampField string,
+	timezone string,
+	parseTimestampFormat string,
+	epochTimestampFormat string,
+	manualTimestampFormat string,
+	parseSeverity bool,
+	severityField string,
+) string {
+	operator := map[string]any{}
+
+	switch parseFormat {
+	case jsonParseFormat:
+		operator = map[string]any{
+			"type":     "json_parser",
+			"parse_to": parseTo,
+		}
+		addSeverityParsingConfig(operator, parseSeverity, parseTo, severityField)
+		addTimestampParsingConfig(operator, parseTimestamp, timezone, parseTo, timestampField, parseTimestampFormat, epochTimestampFormat, manualTimestampFormat)
+	case regexParseFormat:
+		operator = map[string]any{
+			"type":     "regex_parser",
+			"regex":    regexPattern,
+			"parse_to": parseTo,
+		}
+		addSeverityParsingConfig(operator, parseSeverity, parseTo, severityField)
+		addTimestampParsingConfig(operator, parseTimestamp, timezone, parseTo, timestampField, parseTimestampFormat, epochTimestampFormat, manualTimestampFormat)
+	}
+
+	// We marshal as JSON - This seems wrong, because we are using this in a YAML
+	// file, but YAML is actually a superset of JSON, and marshalling json allows for this to be represented on a single
+	// line of a YAML file, so we don't need to worry about indentation.
+	buf := &bytes.Buffer{}
+	_ = operatorMarshalConfig.NewEncoder(buf).Encode(operator)
+
+	bufStr := buf.String()
+
+	if len(bufStr) == 0 {
+		return ""
+	}
+
+	// Remove trailing newline
+	return bufStr[:len(bufStr)-1]
+}
+
+func addSeverityParsingConfig(
+	operator map[string]any,
+	parseSeverity bool,
+	parseTo,
+	severityField string,
+) {
+	if !parseSeverity {
+		return
+	}
+
+	operator["severity"] = map[string]string{
+		"parse_from": fmt.Sprintf("%s.%s", parseTo, severityField),
+	}
+}
+
+func addTimestampParsingConfig(
+	operator map[string]any,
+	parseTimestamp bool,
+	timezone,
+	parseTo,
+	timestampField,
+	parseTimestampFormat,
+	epochFormat,
+	manualFormat string,
+) {
+	if !parseTimestamp {
+		return
+	}
+
+	timestampConfig := map[string]string{
+		"parse_from": fmt.Sprintf("%s.%s", parseTo, timestampField),
+	}
+	switch parseTimestampFormat {
+	case iso8601TimestampFormat:
+		timestampConfig["layout"] = "%Y-%m-%dT%H:%M:%S.%f%z"
+	case rfc3339TimestampFormat:
+		timestampConfig["layout"] = "%Y-%m-%dT%H:%M:%S.%f"
+	case manualTimestampFormat:
+		timestampConfig["layout"] = manualFormat
+	case epochTimestampFormat:
+		timestampConfig["layout_type"] = "epoch"
+		timestampConfig["layout"] = epochFormat
+	}
+
+	timestampConfig["location"] = timezone
+	operator["timestamp"] = timestampConfig
+}
---
+++ resources/helpers/operatorhelper/operators_test.go
@@ -0,0 +1,211 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package operatorhelper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBpRenderStandardParsingOperator(t *testing.T) {
+	testCases := []struct {
+		name                  string
+		parseFormat           string
+		parseTo               string
+		regexPattern          string
+		parseTimestamp        bool
+		timestampField        string
+		timezone              string
+		parseTimestampFormat  string
+		epochTimestampFormat  string
+		manualTimestampFormat string
+		parseSeverity         bool
+		severityField         string
+		expected              string
+	}{
+		{
+			name:        "json parsing only",
+			parseFormat: "json",
+			parseTo:     "body",
+			expected:    `{"parse_to":"body","type":"json_parser"}`,
+		},
+		{
+			name:                 "json + timestamp parsing (ISO8601)",
+			parseFormat:          "json",
+			parseTo:              "body",
+			parseTimestamp:       true,
+			timestampField:       "timestamp",
+			timezone:             "UTC",
+			parseTimestampFormat: "ISO8601",
+			expected:             `{"parse_to":"body","timestamp":{"layout":"%Y-%m-%dT%H:%M:%S.%f%z","location":"UTC","parse_from":"body.timestamp"},"type":"json_parser"}`,
+		},
+		{
+			name:                 "json + timestamp parsing (RFC3339)",
+			parseFormat:          "json",
+			parseTo:              "body",
+			parseTimestamp:       true,
+			timestampField:       "timestamp",
+			timezone:             "UTC",
+			parseTimestampFormat: "RFC3339",
+			expected:             `{"parse_to":"body","timestamp":{"layout":"%Y-%m-%dT%H:%M:%S.%f","location":"UTC","parse_from":"body.timestamp"},"type":"json_parser"}`,
+		},
+		{
+			name:                  "json + timestamp parsing (manual)",
+			parseFormat:           "json",
+			parseTo:               "body",
+			parseTimestamp:        true,
+			timestampField:        "timestamp",
+			timezone:              "UTC",
+			parseTimestampFormat:  "Manual",
+			manualTimestampFormat: "%Y-%m-%d",
+			expected:              `{"parse_to":"body","timestamp":{"layout":"%Y-%m-%d","location":"UTC","parse_from":"body.timestamp"},"type":"json_parser"}`,
+		},
+		{
+			name:                 "json + timestamp parsing (epoch)",
+			parseFormat:          "json",
+			parseTo:              "body",
+			parseTimestamp:       true,
+			timestampField:       "timestamp",
+			timezone:             "UTC",
+			parseTimestampFormat: "Epoch",
+			epochTimestampFormat: "s",
+			expected:             `{"parse_to":"body","timestamp":{"layout":"s","layout_type":"epoch","location":"UTC","parse_from":"body.timestamp"},"type":"json_parser"}`,
+		},
+		{
+			name:          "json + severity",
+			parseFormat:   "json",
+			parseTo:       "body",
+			parseSeverity: true,
+			severityField: "level",
+			expected:      `{"parse_to":"body","severity":{"parse_from":"body.level"},"type":"json_parser"}`,
+		},
+		{
+			name:                 "json + timestamp + severity",
+			parseFormat:          "json",
+			parseTo:              "body",
+			parseTimestamp:       true,
+			timestampField:       "timestamp",
+			timezone:             "UTC",
+			parseTimestampFormat: "ISO8601",
+			parseSeverity:        true,
+			severityField:        "level",
+			expected:             `{"parse_to":"body","severity":{"parse_from":"body.level"},"timestamp":{"layout":"%Y-%m-%dT%H:%M:%S.%f%z","location":"UTC","parse_from":"body.timestamp"},"type":"json_parser"}`,
+		},
+		// REGEXP
+		{
+			name:         "regexp parsing only",
+			parseFormat:  "regex",
+			regexPattern: "^(?P<timestamp>[^ ]*)",
+			parseTo:      "body",
+			expected:     `{"parse_to":"body","regex":"^(?P<timestamp>[^ ]*)","type":"regex_parser"}`,
+		},
+		{
+			name:                 "regex + timestamp parsing (ISO8601)",
+			parseFormat:          "regex",
+			regexPattern:         "^(?P<timestamp>[^ ]*)",
+			parseTo:              "body",
+			parseTimestamp:       true,
+			timestampField:       "timestamp",
+			timezone:             "UTC",
+			parseTimestampFormat: "ISO8601",
+			expected:             `{"parse_to":"body","regex":"^(?P<timestamp>[^ ]*)","timestamp":{"layout":"%Y-%m-%dT%H:%M:%S.%f%z","location":"UTC","parse_from":"body.timestamp"},"type":"regex_parser"}`,
+		},
+		{
+			name:                 "regex + timestamp parsing (RFC3339)",
+			parseFormat:          "regex",
+			regexPattern:         "^(?P<timestamp>[^ ]*)",
+			parseTo:              "body",
+			parseTimestamp:       true,
+			timestampField:       "timestamp",
+			timezone:             "UTC",
+			parseTimestampFormat: "RFC3339",
+			expected:             `{"parse_to":"body","regex":"^(?P<timestamp>[^ ]*)","timestamp":{"layout":"%Y-%m-%dT%H:%M:%S.%f","location":"UTC","parse_from":"body.timestamp"},"type":"regex_parser"}`,
+		},
+		{
+			name:                  "regex + timestamp parsing (manual)",
+			parseFormat:           "regex",
+			regexPattern:          "^(?P<timestamp>[^ ]*)",
+			parseTo:               "body",
+			parseTimestamp:        true,
+			timestampField:        "timestamp",
+			timezone:              "UTC",
+			parseTimestampFormat:  "Manual",
+			manualTimestampFormat: "%Y-%m-%d",
+			expected:              `{"parse_to":"body","regex":"^(?P<timestamp>[^ ]*)","timestamp":{"layout":"%Y-%m-%d","location":"UTC","parse_from":"body.timestamp"},"type":"regex_parser"}`,
+		},
+		{
+			name:                 "regex + timestamp parsing (epoch)",
+			parseFormat:          "regex",
+			regexPattern:         "^(?P<timestamp>[^ ]*)",
+			parseTo:              "body",
+			parseTimestamp:       true,
+			timestampField:       "timestamp",
+			timezone:             "UTC",
+			parseTimestampFormat: "Epoch",
+			epochTimestampFormat: "s",
+			expected:             `{"parse_to":"body","regex":"^(?P<timestamp>[^ ]*)","timestamp":{"layout":"s","layout_type":"epoch","location":"UTC","parse_from":"body.timestamp"},"type":"regex_parser"}`,
+		},
+		{
+			name:          "regex + severity",
+			parseFormat:   "regex",
+			regexPattern:  "^(?P<timestamp>[^ ]*)",
+			parseTo:       "body",
+			parseSeverity: true,
+			severityField: "level",
+			expected:      `{"parse_to":"body","regex":"^(?P<timestamp>[^ ]*)","severity":{"parse_from":"body.level"},"type":"regex_parser"}`,
+		},
+		{
+			name:                 "regex + timestamp + severity",
+			parseFormat:          "regex",
+			regexPattern:         "^(?P<timestamp>[^ ]*)",
+			parseTo:              "body",
+			parseTimestamp:       true,
+			timestampField:       "timestamp",
+			timezone:             "UTC",
+			parseTimestampFormat: "ISO8601",
+			parseSeverity:        true,
+			severityField:        "level",
+			expected:             `{"parse_to":"body","regex":"^(?P<timestamp>[^ ]*)","severity":{"parse_from":"body.level"},"timestamp":{"layout":"%Y-%m-%dT%H:%M:%S.%f%z","location":"UTC","parse_from":"body.timestamp"},"type":"regex_parser"}`,
+		},
+		{
+			name:         "regexp parsing (quote, single quote)",
+			parseFormat:  "regex",
+			regexPattern: `^(?P<timestamp>[^ ]*)'"`,
+			parseTo:      "body",
+			expected:     `{"parse_to":"body","regex":"^(?P<timestamp>[^ ]*)'\"","type":"regex_parser"}`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			out := BpRenderStandardParsingOperator(
+				tc.parseFormat,
+				tc.parseTo,
+				tc.regexPattern,
+				tc.parseTimestamp,
+				tc.timestampField,
+				tc.timezone,
+				tc.parseTimestampFormat,
+				tc.epochTimestampFormat,
+				tc.manualTimestampFormat,
+				tc.parseSeverity,
+				tc.severityField,
+			)
+
+			require.Equal(t, tc.expected, out)
+		})
+	}
+}
